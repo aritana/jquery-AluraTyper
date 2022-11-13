5389ff11d5c0b16f793634b769df58d59e99866a
Aritana Noara Costa Santos <aritanacs@ciandt.com>
2022-11-12 23:21:06 -0300

    restart the game

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,60 @@
-var phrase = $(".phrase").text();
-var words = phrase.split(" ");
-var numberOfWords = words.length;
-var phraseLength = $("#phrase-length");
-
-phraseLength.text(numberOfWords);
-
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
-campo.on("input", function () {
-    var conteudo = campo.val();
 
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $("#contador-palavras").text(qtdPalavras);
-
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres)
-
-});
-
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function () {
-    var cronometroId = setInterval(function () {
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante)
-       console.log(tempoRestante)
-        if (tempoRestante < 1) {
-            campo.attr("disabled", true)
-            clearInterval(cronometroId);
-        }
-
-    }, 1000)
+//To be called when the page is full loaded.
+$(function () {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
 })
 
+
+function atualizaTamanhoFrase() {
+    var phrase = $(".phrase").text();
+    var words = phrase.split(" ");
+    var numberOfWords = words.length;
+    var phraseLength = $("#phrase-length");
+    phraseLength.text(numberOfWords);
+}
+
+function inicializaContadores() {
+    campo.on("input", function () {        
+        var conteudo = campo.val();
+
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres)
+
+    });
+}
+
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function () {
+        $("#botao-reiniciar").attr("disabled", true);
+        var cronometroId = setInterval(function () {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroId);
+                $("#botao-reiniciar").attr("disabled", false);
+            }
+
+        }, 1000)
+    })
+}
+
+function reiniciaJogo() {
+  
+        campo.attr("disabled", false);
+        campo.val("");
+        $("#contador-palavras").text(0);
+        $("#contador-caracteres").text(0);
+        $("#tempo-digitacao").text(tempoInicial);
+        inicializaCronometro();    
+}
+$("#botao-reiniciar").on("click",reiniciaJogo);
